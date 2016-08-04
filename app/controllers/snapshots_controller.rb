75706a8ca9b04d834eecad1da112c49a050e4d05
Rui <ruipserra@gmail.com>
2016-08-04 11:35:20 +0000

    Add data attributes to configure countdown.

--- app/controllers/snapshots_controller.rb
+++ app/controllers/snapshots_controller.rb
@@ -3,6 +3,9 @@ class SnapshotsController < ApplicationController
   def index
     @snapshots = Snapshot.by_insert_order
     @countdown_value = Snapshot::MAX_VAL
+    @countdown_max = Snapshot::MAX_VAL
+    @countdown_min = Snapshot::MIN_VAL
+    @countdown_wait_ms = 1000
   end
 
 end
